eb67c3edf5d6867a65df435135c5600764d4f13a
Kiat Ho <kiatdevacct@gmail.com>
2022-06-23 21:06:30 +0800

    First commit

---
+++ bug.php
@@ -0,0 +1,14 @@
+<h1>Welcome to Simple EHR</h1>
+
+<html>
+<body>
+
+<h2>There is a bug!!! Try logout and login again.<h2>
+<h2>If the problem persist, report to support.<h2>
+
+  <form method="POST" action="index.html">
+    <input type="submit"/ value="Logout">
+  </form>
+
+</body>
+</html>
